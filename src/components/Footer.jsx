2bd6c293817c60f684abccf5c4eb8989381b9799
ukti <uktiagrawal2205@gmail.com>
2022-09-21 20:31:25 +0530

    first commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function footer(){
+    const currentYear=new Date().getFullYear();
+    return <footer><p>Copyright ⓒ {currentYear}</p></footer>
+}
+export default footer;
